892ccfd8b2ddbdd3845127de45b099dfe505f580
matri19 <78371535+matri19@users.noreply.github.com>
2023-05-17 17:40:49 +0200

    Add files via upload

---
+++ data/generated/bib.js
@@ -0,0 +1,101 @@
+﻿define({ entries : {
+    "Ahmadzadeh2023": {
+        "author": "Ahmadzadeh, S. and Ajmal, T. and Ramanathan, R. and Duan, Y.",
+        "doi": "https://doi.org/10.3390/su15043482",
+        "journal": "Sustainability",
+        "number": "4",
+        "pages": "3482",
+        "title": "A Comprehensive Review on Food Waste Reduction Based on IoT and Big Data Technologies",
+        "type": "article",
+        "volume": "15",
+        "year": "2023"
+    },
+    "Cane2020": {
+        "author": "Cane, M. and Parra, C.",
+        "doi": "https://doi.org/10.1108/bfj-06-2019-0391",
+        "journal": "British Food Journal",
+        "note": "ahead-of-print",
+        "title": "Digital platforms: mapping the territory of new technologies to fight food waste",
+        "type": "article",
+        "year": "2020"
+    },
+    "Ciccullo2021": {
+        "author": "Ciccullo, F. and Cagliano, R. and Bartezzaghi, G. and Perego, A.",
+        "doi": "https://doi.org/10.1016/j.resconrec.2020.105114",
+        "journal": "Resources, Conservation and Recycling",
+        "pages": "105114",
+        "title": "Implementing the circular economy paradigm in the agri-food supply chain: The role of food waste prevention technologies",
+        "type": "article",
+        "volume": "164",
+        "year": "2021"
+    },
+    "Endres2022": {
+        "author": "Endres, C.M. and Pelisser, C. and Finco, D.A. and Silveira, M.S. and Piana, V.J.",
+        "doi": "https://doi.org/10.33448/rsd-v11i1.24270",
+        "journal": "Research, Society and Development",
+        "number": "1",
+        "pages": "e0411124270",
+        "title": "IoT and Raspberry Pi application in the food industry: a systematic review",
+        "type": "article",
+        "volume": "11",
+        "year": "2022"
+    },
+    "Hook2022": {
+        "author": "Hook, A. and Soma, T.",
+        "doi": "https://doi.org/10.3389/frsus.2022.1024100",
+        "title": "Sustainability potential of app-based food loss measurement: Farmers\u2019 perspectives in southwestern British Columbia, Canada",
+        "type": "article",
+        "volume": "3",
+        "year": "2022"
+    },
+    "Kamil2022": {
+        "author": "Kamil, M.J.B.M. and Chuah Ee Hua and Sani, M.N.A.",
+        "doi": "https://doi.org/10.24867/JGED-2022-3-005",
+        "journal": "Journal of Graphic Engineering & Design (JGED)",
+        "number": "3",
+        "pages": "5\u201317",
+        "title": "Adaptation of smart-object dimensions in the product design process to reduce household food waste",
+        "type": "article",
+        "volume": "13",
+        "year": "2022"
+    },
+    "MartinRios2018": {
+        "author": "Martin-Rios, C. and Demen-Meier, C. and G\u00f6ssling, S. and Cornuz, C.",
+        "doi": "https://doi.org/10.1016/j.wasman.2018.07.033",
+        "journal": "Waste Management",
+        "pages": "196\u2013206",
+        "title": "Food waste management innovations in the foodservice industry",
+        "type": "article",
+        "volume": "79",
+        "year": "2018"
+    },
+    "Mathisen2022": {
+        "author": "Mathisen, T.F. and Johansen, F.R.",
+        "doi": "https://doi.org/10.2196/38520",
+        "journal": "JMIR Formative Research",
+        "number": "9",
+        "pages": "e38520",
+        "title": "The Impact of Smartphone Apps Designed to Reduce Food Waste on Improving Healthy Eating, Financial Expenses and Personal Food Waste: Crossover Pilot Intervention Trial Studying Students\u2019 User Experiences",
+        "type": "article",
+        "volume": "6",
+        "year": "2022"
+    },
+    "StromAndersen2022": {
+        "author": "Nhat Str\u00f8m-Andersen",
+        "doi": "https://doi.org/10.3920/978-90-8686-939-8_74",
+        "title": "Digital technologies and food waste reduction and prevention: benefits and challenges",
+        "type": "misc",
+        "year": "2022"
+    },
+    "Zmienka2020": {
+        "author": "\u017bmie\u0144ka, E. and Staniszewski, J.",
+        "doi": "https://doi.org/10.2478/manment-2019-0043",
+        "journal": "Management",
+        "number": "1",
+        "pages": "193\u2013207",
+        "title": "Food management innovations for reducing food wastage \u2013 a systematic literature review",
+        "type": "article",
+        "volume": "24",
+        "year": "2020"
+    }
+}});
